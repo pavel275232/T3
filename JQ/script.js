a773b256b43df3a24584f4195cb65fdcd171ab34
Pasha <Pasha27523282@gmail.com>
2022-11-25 13:05:41 +0300

    2

--- JQ/script.js
+++ JQ/script.js
@@ -1,10 +1,22 @@
-var _Seconds = $('.seconds').text(),
-  int;
-int = setInterval(function() { 
-  if (_Seconds > 0) {
-    _Seconds--; 
-    $('.seconds').text(_Seconds);
-  } else {
-    clearInterval(int);
+let m = 29;
+let s = 60;
+$(function timer(){
+
+  setTimeout(timer,1000);
+  if (s>0){
+    s = s - 1;
+
+    console.log(m,s)
+    $('.min').html(m);
+    $('.sec').html(s);
   }
-}, 1000);
+  else{
+    m = m - 1;
+    s = s + 60;
+    $('.sec').html(s);
+  }
+}
+)
+  
+// 
+
